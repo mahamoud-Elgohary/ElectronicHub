4d1be72a5a53a89429aacb47079cd9502c13d118
Roland Raymond Bushra <rolandremon300@gamil.com>
2025-09-10 15:04:06 +0300

    Add Create categories function in database

--- Products/Category.js
+++ Products/Category.js
@@ -1,3 +1,35 @@
-function start(){
+import { db } from "../config.js";
+import{ref, set}from"https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js"
+function CreateCategory(Category_id, Categoryname, Categoryimage) {
+    set(ref(db, 'Categories/' + Category_id), {
+        Categoryname: Categoryname,
+        Categoryimage: Categoryimage
+    })
+    .then(() => {
+        console.log("Category created successfully!");
+    })
+    .catch((error) => {
+        console.error("Error creating category:", error);
+    });
+}
+document.getElementById("create-Category").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-};
+  const Category_id = Date.now().toString(); 
+  const Categoryname = document.getElementById("CN").value;
+  const Categoryimage = document.getElementById("img").value;
+
+  CreateCategory(Category_id, Categoryname, Categoryimage);
+});
+    
+/*function createdb(){
+        Category_id="123456"
+        Categoryname="Mobile";
+        Categoryimage="https://img.global.news.samsung.com/in/wp-content/uploads/2019/01/Galaxy-10yrs_High-Res..jpg";
+        
+        CreateCategory(Category_id,Categoryname,Categoryimage)
+        
+}
+
+createdb()
+*/
