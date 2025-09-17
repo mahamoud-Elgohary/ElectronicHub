062dcb6078438932a86d2b4a8d836fc4c30fe5cb
mahamoud-Elgohary <m.elgohary141999@gmail.com>
2025-09-17 21:01:27 +0300

    Full Responsive and add Product in Landing page add new section for delivery instractions

---
+++ LandingPage/LandingPage.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+//   Burger Menu
+ const toggleBtn = document.getElementById("menu-toggle");
+  const leftSide = document.querySelector(".left-side");
+  const rightSide = document.querySelector(".right-side");
+
+  toggleBtn.addEventListener("click", () => {
+    leftSide.classList.toggle("active");
+    rightSide.classList.toggle("active");
+  });
