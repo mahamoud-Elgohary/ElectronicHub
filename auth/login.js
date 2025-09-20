0e0618b1f8ec715dbea7a70a3e0132b39bf75b85
Ahmed Esam <IAM.AHMEDESAM@GMAIL.COM>
2025-09-20 17:50:58 +0300

    error cart

--- auth/login.js
+++ auth/login.js
@@ -1,15 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 // Same Firebase config as signup.js
 const firebaseConfig = {
-    apiKey: "AIzaSyBWW6-aR7jfNOjNEtJORxEsqjjMJ0cOk3w",
-    authDomain: "electronichub-22676.firebaseapp.com",
-    projectId: "electronichub-22676",
-    storageBucket: "electronichub-22676.appspot.com",
-    messagingSenderId: "104130323974",
-    appId: "1:104130323974:web:9495c1c28539ad872cc587",
-    "hosting": {
+  apiKey: "AIzaSyBWW6-aR7jfNOjNEtJORxEsqjjMJ0cOk3w",
+  authDomain: "electronichub-22676.firebaseapp.com",
+  projectId: "electronichub-22676",
+  storageBucket: "electronichub-22676.appspot.com",
+  messagingSenderId: "104130323974",
+  appId: "1:104130323974:web:9495c1c28539ad872cc587",
+  "hosting": {
     "public": "public",
     "ignore": ["firebase.json", "**/.*", "**/node_modules/**"]
   }
@@ -21,40 +26,39 @@ const auth = getAuth(app);
 const form = document.getElementById("login-form");
 
 form.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-    try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
-  const user = userCredential.user;
-        alert("✅ Welcome back " +user.email);
-        form.reset();
-        // Redirect to dashboard
-        
-               if (user.email.endsWith("@electronichub.com")) {
-                window.location.href = "/homePage/AdminPanel.html";
-                
-}
-  else {
-  window.location.href = "/homePage/home.html";
-}
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    alert("✅ Welcome back " + user.email);
+    form.reset();
+    // Redirect to dashboard
 
-    } catch (error) {
-        window.alert("Login failed: " + error.message);
+    if (user.email.endsWith("@electronichub.com")) {
+      window.location.href = "/homePage/AdminPanel.html";
+
+    } else {
+      window.location.href = "/homePage/home.html";
     }
-    
+
+  } catch (error) {
+    window.alert("Login failed: " + error.message);
+  }
+
 });
 
 
 
 //   Burger Menu
- const toggleBtn = document.getElementById("menu-toggle");
-  const leftSide = document.querySelector(".left-side");
-  const rightSide = document.querySelector(".right-side");
+const toggleBtn = document.getElementById("menu-toggle");
+const leftSide = document.querySelector(".left-side");
+const rightSide = document.querySelector(".right-side");
 
-  toggleBtn.addEventListener("click", () => {
-    leftSide.classList.toggle("active");
-    rightSide.classList.toggle("active");
-  });
+toggleBtn.addEventListener("click", () => {
+  leftSide.classList.toggle("active");
+  rightSide.classList.toggle("active");
+});
